1753c2fcb0eba6a4a7270cf38c175abe2dd6f475
Christian Cantarino <christian.cantarino@gmail.com>
2021-04-13 20:17:41 -0300

    update user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,33 +1,53 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
+  number: {
+    type: Number,
+    required: true
+  },
   name: {
     type: String,
     required: true,
   },
-  cpf: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+  
   phone: {
     type: String,
     required: true,
   },
-  type: {
-    type: Array,
-    required: true,
+  username: {
+    type: String,
+    required: true
   },
   password: {
     type: String,
     required: true,
-    
   },
   email: {
     type: String,
     requireed: true,
     lowercase: true,
     unique: true,
+  },
+  cep: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  complement: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
   },
   createdAt: {
     type: Date,
