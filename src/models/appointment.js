c13ff79a0cb669de558d2ecdfecac324278d3ce5
LucasZatta <lucaszatta5@gmail.com>
2021-04-14 02:36:48 -0300

    filters for appointment screen

--- src/models/appointment.js
+++ src/models/appointment.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const AppointmentSchema = new mongoose.Schema({
-  code: {
-    type: String,
-    required: true,
-  },
   date: {
     type: Date,
     required: true,
